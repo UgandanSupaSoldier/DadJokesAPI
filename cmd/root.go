c0ff86db9571292eea51bac8cb737c2e523ec913
UgandanSupaSoldier <jonathan@directdebit.co.za>
2023-09-08 22:34:44 +0200

    Initial commit of project framework

---
+++ cmd/root.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	//go:embed embeds/banner.txt
+	banner string
+	//go:embed embeds/examples.txt
+	examples string
+)
+
+var rootCmd = &cobra.Command{
+	Use:     "CLI",
+	Long:    banner,
+	Example: examples,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func Init() {}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "DadJokesAPI/cmd"
+
+func main() {
+	cmd.Execute()
+}
